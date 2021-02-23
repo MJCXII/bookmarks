685ab594f18323a4107c4faefc805b4c3325899c
Maximilian Clarke <maximilian-jc@live.com>
2021-02-23 14:56:38 +0000

    added psql DB

--- bookmarks/config.ru
+++
@@ -1,2 +0,0 @@
- require_relative "./manager"
- run Manager
--- bookmarks/spec/features/viewing_bookmarks_spec.rb
+++
@@ -1,8 +0,0 @@
-
-feature 'opening the webpage' do
-  scenario 'viewing the bookmarks' do
-    visit("/")
-    visit("/bookmarks")
-    expect(page).to have_content("Your Bookmarks")
-  end
-end
